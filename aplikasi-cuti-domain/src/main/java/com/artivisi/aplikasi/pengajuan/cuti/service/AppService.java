c1ccbbb51fae0b5b52067771a6c2118582a4e63c
Dadang Iswanto <dadangiswan@gmail.com>
2011-03-16 14:29:06 +0700

    membuat service save or update pegawai, delete pegawai, find pegawai by id, find all pegawai, save or update master konfig, find master konfig, save or update hari libur, delete hari libur, find hari libur by id, find all hari libur, save or update pengajuan cuti, find pengajuan cuti by id, find all pengajuan cuti, save or update realisasi cuti, find realisasi cuti by id, find all realisasi cuti

---
+++ aplikasi-cuti-domain/src/main/java/com/artivisi/aplikasi/pengajuan/cuti/service/AppService.java
@@ -0,0 +1,49 @@
+package com.artivisi.aplikasi.pengajuan.cuti.service;
+
+import java.util.List;
+
+import com.artivisi.aplikasi.pengajuan.cuti.EntryCutiReal;
+import com.artivisi.aplikasi.pengajuan.cuti.MasterHariLibur;
+import com.artivisi.aplikasi.pengajuan.cuti.MasterKonfig;
+import com.artivisi.aplikasi.pengajuan.cuti.Pegawai;
+import com.artivisi.aplikasi.pengajuan.cuti.PengajuanCuti;
+
+public interface AppService {
+	public void savePegawai(Pegawai p);
+
+//service untuk perintah delete
+	public void deletePegawai(Pegawai p);
+
+//service untuk mencari 1 daftar pegawai By ID
+	public Pegawai findPegawaiById(String id);
+	
+//service untuk mencari semua daftar pegawai
+	public List<Pegawai> findAllPegawai();
+	
+	public void saveMasterKonfig(MasterKonfig mk);
+	
+	public MasterKonfig findMasterKonfigById(String id);
+	
+	public void saveMasterHariLibur(MasterHariLibur mhl);
+	
+	public void deleteMasterHariLibur(MasterHariLibur mhl);
+	
+	public MasterHariLibur findMasterHariLiburById(String id);
+	
+	public List<MasterHariLibur> findAllMasterHariLibur();
+	
+	public void savePengajuanCuti(PengajuanCuti pc);
+	
+	public PengajuanCuti findPengajuanCutiById(String id);
+	
+	public List<PengajuanCuti> findAllPengajuanCuti();
+	
+	public void saveEntryCutiReal(EntryCutiReal ecr);
+	
+	public EntryCutiReal findEntryCutiRealById(String id);
+	
+	public List<EntryCutiReal> findAllEntryCutiReal();
+	
+	public EntryCutiReal findPengajuanCutiRealById(String id);
+	
+}
